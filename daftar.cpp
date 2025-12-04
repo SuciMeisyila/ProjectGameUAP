9b18e2ab994ebfc41d406827a3e572e6924ee919
Suci Meisyila <sucimeisyila@gmail.com>
2025-12-04 11:17:29 +0700

    bagian awal

--- daftar.cpp
+++ daftar.cpp
@@ -1,3 +1,53 @@
+#include <ncurses/curses.h>
+#include <cstdlib>
+#include <ctime>
+
+#define SIZE 4
+#define TOTAL (SIZE * SIZE)
+
+// Untuk mengacak array sederhana
+void shuffleArray(int arr[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+void drawBoard(int board[SIZE][SIZE], bool revealed[SIZE][SIZE], int cursorX, int cursorY, int moves, int matchedCount) {
+    clear();
+    mvprintw(0, 0, "=== MEMORY CARD GAME ===");
+    mvprintw(1, 0, "Gunakan panah untuk bergerak, Enter/Spasi untuk membuka, q untuk keluar");
+    mvprintw(2, 0, "Moves: %d    Matched: %d/%d", moves, matchedCount, TOTAL);
+
+    // koordinat start
+    int startY = 4;
+    int startX = 2;
+    for (int y = 0; y < SIZE; y++) {
+        for (int x = 0; x < SIZE; x++) {
+            int px = startX + x * 4;
+            int py = startY + y * 2;
+
+            if (cursorY == y && cursorX == x) {
+                attron(A_REVERSE);
+            }
+
+            if (revealed[y][x]) {
+                // gunakan width 2 untuk menjaga aligned
+                mvprintw(py, px, "%2d", board[y][x]);
+            } else {
+                mvprintw(py, px, " *");
+            }
+
+            if (cursorY == y && cursorX == x) {
+                attroff(A_REVERSE);
+            }
+        }
+    }
+    refresh();
+}
+
 int main() {
     srand((unsigned)time(0));
 
@@ -34,7 +84,7 @@ int main() {
     int matchedCount = 0;
     int moves = 0;
 
-    while (true) {
+    while (true){
         drawBoard(board, revealed, cursorX, cursorY, moves, matchedCount);
 
         if (matchedCount == TOTAL) {
